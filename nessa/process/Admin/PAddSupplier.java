1380891d047ec2aba49e4f9c927c5c89dca9c099
VannessaMasterman <88731038+VannessaMasterman@users.noreply.github.com>
2021-09-26 19:33:39 -0500

    Added Connections to PAddClient and PAddSupplier
    added the basic interactions for adding a client and adding a supplier as an admin

--- nessa/process/Admin/PAddSupplier.java
+++ nessa/process/Admin/PAddSupplier.java
@@ -1,5 +1,6 @@
 package nessa.process.Admin;
 
+import ethanlo.SupplierCollection;
 import nessa.process.UIProcess;
 import nessa.util.ConsoleUtil;
 
@@ -21,7 +22,31 @@ public class PAddSupplier extends UIProcess {
 	@Override
 	public void process(int clientID){
 		if(clientID != -1) return;
-		System.out.println("Performing dummy process " + category + ":" + name.replace(" ", "_"));
-		ConsoleUtil.sleepForSeconds(1.5f);
+		System.out.println();
+		System.out.println("--Adding Supplier--");
+		
+		System.out.print("Supplier Name : ");
+		String name = ConsoleUtil.readLine();
+
+		System.out.print("Supplier Phone Number : ");
+		String phone = ConsoleUtil.readLine();
+
+		System.out.print("Supplier Address : ");
+		String address = ConsoleUtil.readLine();
+
+		System.out.print("Verify the above information is correct (Y/N) : ");
+		String verify = ConsoleUtil.readLine();
+		if(!verify.toLowerCase().contains("y")){
+			// incorrect info, loop until correct
+			process(clientID);
+			return; // don't continue for any recursive call	
+		}
+		// verified correct info
+		// add client
+		SupplierCollection.instance().addSupplier(name, phone, address);
+		// inform user client added
+		System.out.println("Client \"" + name + "\" has been added successfully");
+		// wait on message for 0.2 seconds so user can read briefly
+		ConsoleUtil.sleepForSeconds(0.2f);
 	}
 }
